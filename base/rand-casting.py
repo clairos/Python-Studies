50372a4892f783b51b83f6fd084db51b320aef4f
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-02 14:50:06 -0300

    add final base + start strings

---
+++ base/rand-casting.py
@@ -0,0 +1,11 @@
+import random
+
+numi=10
+numf=5.2
+numc=1j
+
+numr=random.randrange(0,59) # numero aleatorio entre 0 e 59
+
+x=numr 
+
+print("Valor: " + str(x) + " -- Tipo: " + str(type(x)))
--- base/tipos-dados.py
+++ base/tipos-dados.py
@@ -5,7 +5,8 @@ x=False #boolean
 
 n1=5; n2=2; x=complex(n1, n2)
 x=["Carro", "Aviao", "Navio"] # list / array -> valores podem ser alterados
-x[0]="Onibus"
+x[0]="Onibus" # alterado o valor na posicao 0 da lista
+
 x=("Carro", "Aviao", "Navio", 1, 58.3, True) # tuple -> valores nao podem ser alterados
 
 x=range(0,100,1) # cria automaticamente uma lista
@@ -19,7 +20,7 @@ x={ #dict
 x={1,3,4,5,7,1,4,5} #set -> nao imprime valores repetidos
 x=frozenset({1,3,4,5,7,1,4,5}) # set, porem nao permite alteração
 
-print("Valor: " + str(x))
+print("Valor: " + str(x)) # str() converte para string
 print("Valor: " + str(x[0]))
 print("Valor: " + x["languague"])
 print("Tipo: " + str(type(x)))
