fa117a1152f1323ca436b5f9a51896dbf9b4244d
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-02 17:40:59 -0300

    joguinho usando random + .format

---
+++ jogo simples - adivinhacao/app.py
@@ -0,0 +1,19 @@
+import random
+import os
+
+erros = 0
+sorteado=random.randrange(0,100)
+jogador=int(input("Digite o seu número: "))
+
+while (sorteado != jogador):
+    os.system('cls')
+
+    if(sorteado > jogador):
+        print("ERROU, o número é maior")
+    else:
+        print("ERROU, o número é menor")
+    
+    erros+=1
+    jogador=int(input("Digite seu número: "))
+
+print("Numero {}, você acertou em {} tentativas".format(jogador, erros))
