810e62b378b2439918114f5a982d0a8516798879
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-02 16:21:59 -0300

    if elif e else

---
+++ condicionais/elif-else/condicional.py
@@ -0,0 +1,32 @@
+a=10; b=5; res=0
+op=" * "
+
+if "+" in op:
+    res=a+b
+
+elif "-" in op: # elif = else + if
+    res=a-b
+
+elif "*" in op:
+    res=a*b
+
+elif "/" in op:
+    res=a/b
+
+else:
+    print("Operador Inválido")
+
+print(str(a) + op + str(b) + " = " + str(res))
+
+
+clima="chuva"; dinheiro=299; lugar=""
+
+if clima=="sol" or (dinheiro>300 and dinheiro <500):
+    lugar="clube"
+else:
+    lugar="cinema"
+
+print("Vou ao " + lugar)
+
+# and = tudo tem que ser True para retornar True
+# or = apenas um tem que ser True para retornar False
