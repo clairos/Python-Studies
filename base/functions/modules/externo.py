3f7f6f91e440cb25556564251259cc806dc43f86
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-06 14:57:41 -0300

    import arquivo externo

---
+++ base/functions/modules/externo.py
@@ -0,0 +1,9 @@
+def curso():
+    print("Curso de Python")
+
+char={
+    "nome":"jake",
+    "pontos":100,
+    "energia":100,
+    "time":1
+}
---
+++ base/functions/modules/main.py
@@ -0,0 +1,11 @@
+import externo as ex # importa a biblioteca inteira
+# from externo import char # importa somente parte da biblioteca
+
+# ex.curso()
+
+# print(ex.char["nome"])
+# print(char["nome"])
+
+res=dir(ex)
+
+print(res)
