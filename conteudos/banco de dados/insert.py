f5386059814b2170a0ec2717078020c5cc46ab8b
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-08 17:22:25 -0300

    delete/insert/update

---
+++ conteudos/banco de dados/insert.py
@@ -0,0 +1,33 @@
+import sqlite3
+from sqlite3 import Error
+
+def dbConnection(): # funcao para fazer a conexao com o banco de dados
+    path="C:\\Users\\clara.brusa\\Desktop\Python\\banco\\agenda.db"
+    con=None # connection
+
+    try:
+        con=sqlite3.connect(path)
+
+    except Error as ex:
+        print(ex)
+    
+    return con
+
+vcon=dbConnection()
+
+nome=input("Digite o nome: ")
+tel=input("Digite o telefone: ")
+email=input("Digite o email: ")
+
+vsql="INSERT INTO tb_contatos (T_NOMECONTATO, T_TELEFONE, T_EMAILCONTATO) VALUES ('"+nome+"','"+tel+"','"+email+"')"
+
+def insert(conexao, sql):
+    try:
+        c=conexao.cursor()
+        c.execute(sql)
+        conexao.commit()
+        print("Registro inserido")
+    except Error as ex:
+        print(ex)
+
+insert(vcon, vsql)
