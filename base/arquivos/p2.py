6da52f685d798e4acfeb3d19dd349795a881ecf8
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-08 15:19:51 -0300

    os.path

---
+++ base/arquivos/p2.py
@@ -0,0 +1,18 @@
+import re
+import os
+
+nome = "teste-2.txt"
+caminho = "C:/Users/clara.brusa/Desktop/Python/base/arquivos/"
+
+if os.path.exists(caminho+nome):
+    print("Arquivo existente")
+else: 
+    f=open(caminho+nome, "x")
+    f.close()
+    print("Arquivo criado")
+
+if os.path.exists(caminho+nome):
+    os.remove(caminho+nome)
+    print("Arquivo removido")
+else:
+    print("Arquivo inexistente")
