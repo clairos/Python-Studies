ddf5a309debbb8ea06b78deecb66823607bd85f3
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-13 15:45:31 -0300

    ajuste de erro urllib p2

--- conteudos/urllib/p2.py
+++ conteudos/urllib/p2.py
@@ -5,13 +5,12 @@ texto = pagina.read().decode('utf-8')
 
 produto = input('Digite o produto: ')
 produto_pos = texto.find(produto)
-
-produto_txt = texto[produto_pos:1000]
-preco_pos = produto.text.find('R$')
-preco = texto[produto_pos + preco_pos : produto_pos + preco_pos + 7]
+produto_txt = texto[produto_pos: 1000]
+preco_pos = produto_txt.find('R$')
+preco = texto[produto_pos + preco_pos: produto_pos + preco_pos + 7]
 
 if produto_pos > -1:
-    print(f"Produto: {produto}")
-    print(f"Preço..: {preco}")
+    print(f"Produto.: {produto}")
+    print(f"Preço...: {preco}")
 else:
-    print("Produto não encontrado!")
+    print("Produto não encontrado")
