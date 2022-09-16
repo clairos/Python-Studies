f2320520629b3d35f23fca5815edd6f8d5387aee
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-16 15:00:44 -0300

    arrumei, usando waitress ao inves de gunicorn

--- curso-flask/aula3/wsgi.py
+++ curso-flask/aula3/wsgi.py
@@ -1,11 +1,6 @@
 def application(environ, start_response):
-    body = "Hello, World!"
+    body = b"Hello, World!"
     status = "200 OK"
-    headers = [{"Content-Type": "text/plain"}]
+    headers = [("Content-type", "text/plain")]
     start_response(status, headers)
     return [body]
-
-if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-
-    make_server("0.0.0.0", 8000, application).serve_forever()
