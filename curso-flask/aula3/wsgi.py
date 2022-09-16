7f904549b78414915166e038114e3728d811441c
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-16 15:09:17 -0300

    wsgi com html

--- curso-flask/aula3/wsgi.py
+++ curso-flask/aula3/wsgi.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
-    body = b"Hello, World!"
+    body = b"<h1>Hello World</h1><button style='cursor: pointer'>click</button>"
     status = "200 OK"
-    headers = [("Content-type", "text/plain")]
+    headers = [("Content-type", "text/html")]
     start_response(status, headers)
     return [body]
