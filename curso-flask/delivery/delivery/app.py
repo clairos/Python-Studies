197a2ff2045df12a15eb9dd8d22b100623d1692c
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-20 16:22:09 -0300

    exemplo index.html

--- curso-flask/delivery/delivery/app.py
+++ curso-flask/delivery/delivery/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 def create_app():
     app = Flask(__name__)
 
     @app.route("/")
     def index():
-        return render_template('index.html')
+        return render_template(
+            'index.html',
+            name = request.args['name']
+        )
 
     return app
