2f03edc894d3ed7b158be525eb2f92b36cdd0763
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-22 16:46:14 -0300

    aproveitamento de blocos de codigo html

--- curso-flask/delivery/delivery/ext/site/main.py
+++ curso-flask/delivery/delivery/ext/site/main.py
@@ -6,3 +6,7 @@ bp = Blueprint("site", __name__)
 @bp.route("/")
 def index():
     return render_template('index.html')
+
+@bp.route("/about")
+def about():
+    return render_template('about.html')
