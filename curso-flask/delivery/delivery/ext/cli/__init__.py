74c91eda17fe32972ddc566034b8c70483d6d019
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-23 16:26:36 -0300

    add usuario no db

--- curso-flask/delivery/delivery/ext/cli/__init__.py
+++ curso-flask/delivery/delivery/ext/cli/__init__.py
@@ -1,6 +1,6 @@
 import click
 from delivery.ext.db import db
-from delivery.ext.site import models #noqa
+from delivery.ext.site import models 
 
 
 def init_app(app):
@@ -9,6 +9,22 @@ def init_app(app):
     def create_db():
         # este comando inicializa o banco de dados
         db.create_all()
+
+    @app.cli.command()
+    @click.option("--email", "-e")
+    @click.option("--password", "-p")
+    @click.option("--admin", "-a", is_flag=True, default=False)
+    def add_user(email, password, admin):
+        # adiciona novo usuario
+        user = models.User(
+            email=email,
+            password=password,
+            admin=admin
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        click.echo("New user was successfully added!")
     
     @app.cli.command()
     def list_orders():
