a5bfae1d60f221b467235a07e5dc94bbadbb131f
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-23 15:28:21 -0300

    configuracao db

--- curso-flask/delivery/delivery/app.py
+++ curso-flask/delivery/delivery/app.py
@@ -3,11 +3,13 @@ from flask import Flask
 from delivery.ext import site
 from delivery.ext import config
 from delivery.ext import toolbar
+from delivery.ext import db
 
 def create_app():
     app = Flask(__name__)
     
     config.init_app(app)
+    db.init_app(app)
     toolbar.init_app(app)
     site.init_app(app)
 
