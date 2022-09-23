52b5fd5c695818720c4a972d4e2f712f7dc58cb5
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-23 15:38:32 -0300

    adicionado novos comandos flask

---
+++ curso-flask/delivery/delivery/ext/cli/__init__.py
@@ -0,0 +1,18 @@
+import click
+from delivery.ext.db import db
+
+
+def init_app(app):
+
+    @app.cli.command()
+    def create_db():
+        # este comando inicializa o banco de dados
+        db.create_all()
+    
+    @app.cli.command()
+    def list_orders():
+        click.echo("list orders")
+
+    @app.cli.command()
+    def list_users():
+        click.echo("list users")
