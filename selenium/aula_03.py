674f7f8df635b27aba1e8f9b6647219c916a1438
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-10-20 15:53:46 -0300

    ajustado com sleep!

--- selenium/aula_03.py
+++ selenium/aula_03.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
 # cria a instância do driver do navegador
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -14,9 +15,14 @@ url = "https://curso-python-selenium.netlify.app/aula_03.html"
 
 driver.get(url)
 
-driver.implicitly_wait(20)
+sleep(3)
 
 a = driver.find_element(By.TAG_NAME, "a")
+a.click()
 
+p = driver.find_element(By.TAG_NAME, "p")
 
-# driver.quit()
+print(f"texto de a: {a.text}")
+print(f"texto de p: {p.text}")
+
+driver.quit()
