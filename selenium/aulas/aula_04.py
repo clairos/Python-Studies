2eda084f2b7320977308501e4a1d96c118b4276f
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-10-21 10:56:14 -0300

    init aula 04_a

---
+++ selenium/aulas/aula_04.py
@@ -0,0 +1,21 @@
+# aninhada
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+# cria a instância do driver do navegador
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+wdw = WebDriverWait(driver, 10)
+
+url = "https://curso-python-selenium.netlify.app/aula_04.html"
+
+driver.get(url)
+
+sleep(1)
+
