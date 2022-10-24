29f158fbb561194e1ea55521c12969bd7c9fdac2
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-10-24 16:41:55 -0300

    04_b

--- selenium/aulas/aula_04_b.py
+++ selenium/aulas/aula_04_b.py
@@ -1,5 +1,3 @@
-# aninhada
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -8,6 +6,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+
+def find_by_text(driver, tag, texto):
+    elementos = driver.find_elements(By.TAG_NAME, tag) # lista
+
+    for elemento in elementos:
+        if elemento.text == texto:
+            return elemento
 
 # cria a instância do driver do navegador
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -18,5 +23,23 @@ url = "https://selenium.dunossauro.live/aula_04_b.html"
 driver.get(url)
 
 sleep(1)
+
+nomes_das_caixas = ['um', 'dois', 'tres', 'quatro']
+
+for nome in nomes_das_caixas:
+    sleep(0.3)
+    find_by_text(driver, 'div', nome).click()
+
+# sleep(1)
+
+for nome in nomes_das_caixas:
+    sleep(0.3)
+    driver.back()
+
+for nome in nomes_das_caixas:
+    sleep(0.3)
+    driver.forward()
+
+sleep(0.5)
 
 driver.quit()
