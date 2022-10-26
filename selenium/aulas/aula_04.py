076c063d121d5019d2d8126aa303ac61e209a8cf
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-10-26 16:03:11 -0300

    aula 04 'desafio' start

--- selenium/aulas/aula_04.py
+++ selenium/aulas/aula_04.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+from pprint import pprint # pretty print
 
 # cria a instância do driver do navegador
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -16,5 +17,27 @@ url = "https://curso-python-selenium.netlify.app/aula_04.html"
 driver.get(url)
 
 sleep(1)
+
+aside = driver.find_element(By.TAG_NAME, 'aside')
+
+aside_as = aside.find_elements(By.TAG_NAME, 'a')
+
+links_aulas = {}
+
+for a in aside_as:
+    links_aulas[a.text] = [a.get_attribute('href')]
+
+pprint(links_aulas)
+aula_3 = str(links_aulas['Aula 3']).split("'")
+new_url = aula_3[1]
+print(new_url)
+
+driver.get(new_url)
+
+driver.back()
+
+# main = driver.find_element(By.TAG_NAME, 'main')
+
+
 
 driver.quit()
