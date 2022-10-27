7dfdc77702fafc8c53e25b5581826e8b88f5fa67
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-10-27 14:12:11 -0300

    exercicio 3 finalizado!

--- selenium/exercicios/ex3.py
+++ selenium/exercicios/ex3.py
@@ -8,33 +8,63 @@ from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from pprint import pprint # pretty print
 
+# retorna um dicionario com os link das tags 'a' dentro de outra tag (ex. 'main')
+def get_link(driver, elemento): 
+    result = {}
+
+    element = driver.find_element(By.TAG_NAME, elemento)
+    ancoras = element.find_elements(By.TAG_NAME, 'a')
+
+    for ancora in ancoras:
+        result[ancora.text] = ancora.get_attribute('href')
+    
+    return result
+
+# retorna elemento que tenha o link especifico no atributo 'href'
+def find_c_e(driver, link):
+    elementos = driver.find_elements(By.TAG_NAME, 'a')
+
+    for elemento in elementos:
+        if link in elemento.get_attribute('href'):
+            return elemento
+
 # cria a instância do driver do navegador
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 wdw = WebDriverWait(driver, 10)
 
 url = "https://curso-python-selenium.netlify.app/exercicio_03.html"
 
+#entra na URL do exercicio 3
 driver.get(url)
-
 sleep(1)
 
-def find_links(driver, elemento):
-    element = driver.find_element(By.TAG_NAME, elemento)
-    a = element.find_elements(By.TAG_NAME, 'a')
-    return a
+#começa o exercício
+start = get_link(driver, 'main')
+driver.get(start['Começar por aqui'])
 
-a1 = driver.find_elements(By.TAG_NAME, 'a')
-print(a1[-1].text)
-a1[-1].click()
+# pagina 1
+sleep(0.8)
+errado = find_c_e(driver, 'page_2')
+click_e = errado.get_attribute('href')
+driver.get(click_e)
 
-sleep(1)
+# pagina 2
+sleep(0.8)
+certo = find_c_e(driver, 'page_3')
+click_c = certo.get_attribute('href')
+driver.get(click_c)
 
-a2 = find_links(driver, 'main')
+# pagina 3
+sleep(0.8)
+certo = find_c_e(driver, 'page_4')
+click_c = certo.get_attribute('href')
+driver.get(click_c)
 
-for a in a2:
-    if a.get_attribute('attr') == 'errado':
-        a.click()
+# pagina 4
+sleep(0.8)
+driver.refresh()
 
-sleep(1)
+# fim do jogo!
+sleep(0.8)
 
 driver.quit()
