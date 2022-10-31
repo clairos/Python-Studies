633c1d0991a8f0464f9d66852cdd0530d86edff7
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-10-31 10:38:19 -0300

    comparação

--- selenium/aulas/aula_05.py
+++ selenium/aulas/aula_05.py
@@ -6,26 +6,46 @@ from webdriver_manager.chrome import ChromeDriverManager  # type: ignore
 from selenium.webdriver.support.ui import WebDriverWait  # type: ignore
 from selenium.webdriver.support import expected_conditions as EC  # type: ignore
 from time import sleep
+from urllib.parse import urlparse
+from json import loads
 
 # cria a instância do driver do navegador
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 wdw = WebDriverWait(driver, 10)
 
-url = "https://curso-python-selenium.netlify.app/aula_05.html"
+url = 'https://curso-python-selenium.netlify.app/aula_05.html'
 
 def preenche_form(browser, nome, email, senha, telefone):
-    browser.find_element(By.NAME, "nome").send_keys(nome)
-    browser.find_element(By.NAME, "email").send_keys(email)
-    browser.find_element(By.NAME, "senha").send_keys(senha)
-    browser.find_element(By.NAME, "telefone").send_keys(telefone)
-    browser.find_element(By.NAME, "btn").click()
+    browser.find_element(By.NAME, 'nome').send_keys(nome)
+    browser.find_element(By.NAME, 'email').send_keys(email)
+    browser.find_element(By.NAME, 'senha').send_keys(senha)
+    browser.find_element(By.NAME, 'telefone').send_keys(telefone)
+    browser.find_element(By.NAME, 'btn').click()
 
 driver.get(url)
 
 sleep(1)
 
-preenche_form(driver, "Clara", "clarasgore@gmail.com", "senha1234", "49999024915")
+estrutura = {
+    'nome': 'Clara',
+    'email': 'clara@gmail.com',
+    'senha': 'senha1234',
+    'telefone': '49999024915'
+}
 
-# sleep(3)
+preenche_form(driver, **estrutura)
+
+# print(urlparse(driver.current_url))
+sleep(1)
+
+texto_result = driver.find_element(By.ID, 'result').text
+result_arrumado = texto_result.replace('\'', '\"')
+
+dic_result = loads(result_arrumado)
+
+if dic_result==estrutura:
+    print('deu certo!')
+else:
+    print('não deu certo :(')
 
 driver.quit()
