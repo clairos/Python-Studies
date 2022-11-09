7d1b1290cb08aafc701cc46747e723881137e1a1
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-11-09 08:15:16 -0300

    ignore pylance aulas

--- selenium/aulas/aula_04.py
+++ selenium/aulas/aula_04.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service # type: ignore
+from selenium.webdriver.common.by import By # type: ignore
+from selenium.webdriver.common.keys import Keys # type: ignore
+from webdriver_manager.chrome import ChromeDriverManager # type: ignore
+from selenium.webdriver.support.ui import WebDriverWait # type: ignore
+from selenium.webdriver.support import expected_conditions as EC # type: ignore
 from time import sleep
 from pprint import pprint # pretty print
 
