1613b20b03aa31680210881ddc9074263f369f5e
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-11-22 10:33:16 -0300

    aula 07 b

--- selenium/aulas/aula_07_b.py
+++ selenium/aulas/aula_07_b.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service  # type: ignore
 from selenium.webdriver.common.by import By  # type: ignore
-from selenium.webdriver.common.keys import Keys  # type: ignore
+# from selenium.webdriver.common.keys import Keys  # type: ignore
 from webdriver_manager.chrome import ChromeDriverManager  # type: ignore
 from selenium.webdriver.support.ui import WebDriverWait  # type: ignore
 from selenium.webdriver.support import expected_conditions as EC  # type: ignore
@@ -11,9 +11,15 @@ from time import sleep
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 wdw = WebDriverWait(driver, 10)
 
-url = 'https://curso-python-selenium.netlify.app/aula_07.html'
+url = 'https://curso-python-selenium.netlify.app/aula_07_b.html'
 
 driver.get(url)
+
+sleep(1)
+
+btn = driver.find_element(By.CSS_SELECTOR, 'button')
+
+btn.click()
 
 sleep(1)
 
