d6371858df096d5db60a089ba831585cc3d64b33
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-11-25 09:15:56 -0300

    arquivo base para aula 8, ajuste de imports aula 7

--- selenium/aulas/aula_07.py
+++ selenium/aulas/aula_07.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service  # type: ignore
 from selenium.webdriver.common.by import By  # type: ignore
-
 # from selenium.webdriver.common.keys import Keys  # type: ignore
 from webdriver_manager.chrome import ChromeDriverManager  # type: ignore
 from selenium.webdriver.support.ui import WebDriverWait  # type: ignore
